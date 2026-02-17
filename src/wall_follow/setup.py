b3995be220fdc646efb87dc14e44a1fbce794f04
oloyedegbemiga <gbemigao@seas.upenn.edu>
2026-02-16 19:09:37 -0500

    initial commit for wall follow. Just printing approp. commands

--- src/wall_follow/setup.py
+++ src/wall_follow/setup.py
@@ -27,3 +27,33 @@ setup(
         ],
     },
 )
+from setuptools import find_packages, setup
+
+package_name = 'wall_follow'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='bemi',
+    maintainer_email='gbemigao@seas.upenn.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+entry_points={
+    'console_scripts': [
+        'lidar_wallfollow_debug = wall_follow.lidar_wallfollow_debug:main',
+    ],
+},
+)
