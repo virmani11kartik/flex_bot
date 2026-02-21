5d965ac5dea1601b9995024bf7963d40ab0e1d72
virmani11kartik <kartik8virmani@gmail.com>
2026-02-20 21:15:15 -0500

    02/21 updates

--- src/wall_follow/setup.py
+++ src/wall_follow/setup.py
@@ -37,23 +37,22 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='bemi',
     maintainer_email='gbemigao@seas.upenn.edu',
-    description='TODO: Package description',
+    description='Wall following using LaserScan and wheel rad/s commands',
     license='TODO: License declaration',
     extras_require={
-        'test': [
-            'pytest',
+        'test': ['pytest'],
+    },
+    entry_points={
+        'console_scripts': [
+            'wall_follow = wall_follow.wall_follow:main',
+            'lidar_wallfollow_debug = wall_follow.lidar_wallfollow_debug:main',
         ],
     },
-entry_points={
-    'console_scripts': [
-        'lidar_wallfollow_debug = wall_follow.lidar_wallfollow_debug:main',
-    ],
-},
 )
