2e2f9f6d1027c9db81aae8ec2328a58401a2589b
oloyedegbemiga <gbemigao@seas.upenn.edu>
2026-02-27 18:02:04 -0500

    wall follow code for jetson

--- src/wall_follow/setup.py
+++ src/wall_follow/setup.py
@@ -8,51 +8,23 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='bemi',
     maintainer_email='gbemigao@seas.upenn.edu',
-    description='Wall following using LaserScan and wheel rad/s commands',
+    description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
-        'test': ['pytest'],
-    },
-    entry_points={
-        'console_scripts': [
-            'wall_follow = wall_follow.wall_follow:main',
-            'lidar_wallfollow_debug = wall_follow.lidar_wallfollow_debug:main',
+        'test': [
+            'pytest',
         ],
     },
-)
-from setuptools import find_packages, setup
-
-package_name = 'wall_follow'
-
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+entry_points={
+    'console_scripts': [
+        'lidar_wallfollow_debug = wall_follow.lidar_wallfollow_debug:main',
     ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='bemi',
-    maintainer_email='gbemigao@seas.upenn.edu',
-    description='Wall following using LaserScan and wheel rad/s commands',
-    license='TODO: License declaration',
-    extras_require={
-        'test': ['pytest'],
-    },
-    entry_points={
-        'console_scripts': [
-            'wall_follow = wall_follow.wall_follow:main',
-            'lidar_wallfollow_debug = wall_follow.lidar_wallfollow_debug:main',
-        ],
-    },
+},
 )
